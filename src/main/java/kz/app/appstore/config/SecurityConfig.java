6fa0d273abff2d1782eab3d27d6a266dad786e35
shahruhisaev <isaevshahruh2001@gmail.com>
2024-11-17 12:58:12 +0500

    added new request matcher;

--- src/main/java/kz/app/appstore/config/SecurityConfig.java
+++ src/main/java/kz/app/appstore/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/manager/**").hasRole("MANAGER")
+                        .requestMatchers("/api/worker/**").hasRole("WAREHOUSE_WORKER")
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
