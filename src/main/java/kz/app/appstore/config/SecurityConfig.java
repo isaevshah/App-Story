980a4b507e446e3c4dfd6ef75600ec2fea815ab3
shahruhisaev <isaevshahruh2001@gmail.com>
2024-11-24 14:31:07 +0500

    added cors config;

--- src/main/java/kz/app/appstore/config/SecurityConfig.java
+++ src/main/java/kz/app/appstore/config/SecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableMethodSecurity
@@ -41,6 +44,18 @@ public class SecurityConfig {
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("https://app-story-production.up.railway.app"); // Разрешаем запросы со всех доменов (для тестирования)
+        configuration.addAllowedMethod("*"); // Разрешаем все методы (GET, POST, DELETE и т.д.)
+        configuration.addAllowedHeader("*"); // Разрешаем все заголовки
+        configuration.setAllowCredentials(true); // Разрешаем отправку куки
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
