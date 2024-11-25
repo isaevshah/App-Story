bdd03a4f0e441ea41357c08d749fb0987321e62d
shahruhisaev <isaevshahruh2001@gmail.com>
2024-11-26 00:25:55 +0500

    deleted unused imports;

--- src/main/java/kz/app/appstore/config/SecurityConfig.java
+++ src/main/java/kz/app/appstore/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package kz.app.appstore.config;
 
-import kz.app.appstore.service.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.*;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.*;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.*;
@@ -14,9 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableMethodSecurity
