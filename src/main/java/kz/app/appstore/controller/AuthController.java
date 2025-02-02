5a0969b1007786430d0c7e7b618eb6e4ca0b6eb7
shahruhisaev <isaevshahruh2001@gmail.com>
2025-02-02 20:25:10 +0500

    changed requestMapping auth controller 1;

--- src/main/java/kz/app/appstore/controller/AuthController.java
+++ src/main/java/kz/app/appstore/controller/AuthController.java
@@ -20,7 +20,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/shop-service/api/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
