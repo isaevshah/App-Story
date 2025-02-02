b39fb9e93b649a7b31293a86bab86efb7e870741
shahruhisaev <isaevshahruh2001@gmail.com>
2025-02-02 21:05:58 +0500

    added security in swagger config;

--- src/main/java/kz/app/appstore/config/SwaggerConfig.java
+++ src/main/java/kz/app/appstore/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package kz.app.appstore.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,14 +14,21 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-//                .addServersItem(new Server().url("/shop-service"))
+                .addServersItem(new Server().url("/payment-service"))
                 .info(new Info()
-                        .title("Shop Service API")
-                        .description("API для свободного пользования внешними организациями для получения различных услуг")
+                        .title("Online Bank Payment Service API")
+                        .description("API для управления пользователем Онлайн-банкинга")
                         .version("0.0.1")
                         .contact(new Contact()
-                                .name("Shahruh Isaev")
-                                .email("shahruhi@bankffin.kz")));
+                                .name("Isaev Shahruh")
+                                .email("isaevshahruh2001@gmail.com")))
+                // Define multiple security requirements for the API
+                .components(new io.swagger.v3.oas.models.Components()
+                        // Define Bearer Auth (JWT)
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 
     @Bean
