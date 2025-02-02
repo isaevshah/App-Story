65169e3f1627074c1539eb2a77a1149ff05cd068
shahruhisaev <isaevshahruh2001@gmail.com>
2025-02-02 21:09:23 +0500

    added security in swagger config 1;

--- src/main/java/kz/app/appstore/config/SwaggerConfig.java
+++ src/main/java/kz/app/appstore/config/SwaggerConfig.java
@@ -1,10 +1,11 @@
 package kz.app.appstore.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,21 +15,21 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("/payment-service"))
                 .info(new Info()
-                        .title("Online Bank Payment Service API")
-                        .description("API для управления пользователем Онлайн-банкинга")
+                        .title("Shop Service API")
+                        .description("API для свободного пользования внешними организациями для получения различных услуг")
                         .version("0.0.1")
                         .contact(new Contact()
-                                .name("Isaev Shahruh")
-                                .email("isaevshahruh2001@gmail.com")))
-                // Define multiple security requirements for the API
-                .components(new io.swagger.v3.oas.models.Components()
-                        // Define Bearer Auth (JWT)
-                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")));
+                                .name("Shahruh Isaev")
+                                .email("shahruhi@bankffin.kz")))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth")) // ⬅️ Добавляем секьюрность
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .name("BearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT"))); // ⬅️ Конфигурируем Bearer Token
     }
 
     @Bean
